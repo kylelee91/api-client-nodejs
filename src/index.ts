0955806d7ddf8c0f4633281c48c3dd07932370c4
amattoni <amattoni@petrichor.io>
2016-08-21 22:58:53 -0400

    simplify imports

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,21 @@
 // Cycle API
-import * as Accounts from "./modules/accounts/index";
-import * as Billing from "./modules/billing/index";
-import * as Containers from "./modules/containers/index";
-import * as DataCenters from "./modules/datacenters/index";
-import * as Dns from "./modules/dns/index";
-import * as Environments from "./modules/environments/index";
-import * as Images from "./modules/images/index";
-import * as Jobs from "./modules/jobs/index";
-import * as Plans from "./modules/plans/index";
-import * as Repos from "./modules/repos/index";
-import * as Teams from "./modules/teams/index";
-import * as Tiers from "./modules/tiers/index";
+import * as Accounts from "./modules/accounts";
+import * as Billing from "./modules/billing";
+import * as Containers from "./modules/containers";
+import * as DataCenters from "./modules/datacenters";
+import * as Dns from "./modules/dns";
+import * as Environments from "./modules/environments";
+import * as Images from "./modules/images";
+import * as Jobs from "./modules/jobs";
+import * as Plans from "./modules/plans";
+import * as Repos from "./modules/repos";
+import * as Teams from "./modules/teams";
+import * as Tiers from "./modules/tiers";
 
-import * as Auth from "./auth/index";
+import * as Auth from "./auth";
 import * as Errors from "./common/errors";
 import * as Structures from "./common/structures";
-import * as JsonApi from "./jsonapi/index";
+import * as JsonApi from "./jsonapi";
 import * as Request from "./common/request";
 
 export { QueryParams } from "./common/request";
