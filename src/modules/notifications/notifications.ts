48ba742cc7353d350b05bcf329d7f1305cd9bc08
amattoni <amattoni@petrichor.io>
2016-09-09 23:30:44 -0400

    Update notification structure

--- src/modules/notifications/notifications.ts
+++ src/modules/notifications/notifications.ts
@@ -1,5 +1,5 @@
 import * as JsonApi from "../../jsonapi/index";
-import { Id, Scope, Time, Events, State } from "../../common/structures";
+import { Id, Scope, Time, Events, State, Task } from "../../common/structures";
 import * as ApiRequest from "../../common/request";
 
 export function document(): typeof CollectionRequest;
@@ -30,8 +30,9 @@ export interface Resource extends JsonApi.Resource {
         message: string;
         owner: Scope;
         creator: Id;
-        events: Events & {read: Time};
+        events: Events & { read: Time };
         state: State<States>
+        type: Types;
     };
 }
 
@@ -40,10 +41,26 @@ export interface CollectionMeta {
 }
 
 export type States = "new" | "read" | "hidden";
+export type Types = "success" | "error" | "warning" | "info";
 
+export type CollectionActions = "mark_read";
 export class CollectionRequest {
+    public static readonly target = "notifications";
+
     public static async get(query?: ApiRequest.QueryParams): Promise<Collection> {
-        return ApiRequest._get<Collection>("notifications", query);
+        return ApiRequest._get<Collection>(this.target, query);
+    }
+
+    public static async markAllAsRead() {
+        return this.task(new Task("mark_read"));
+    }
+
+    public static async task(t: Task<CollectionActions>, query?: ApiRequest.QueryParams): Promise<Task<CollectionActions>> {
+        return ApiRequest._post<Task<CollectionActions>>(
+            `${this.target}/tasks`,
+            t,
+            query
+        );
     }
 }
 
@@ -52,5 +69,17 @@ export class SingleRequest {
 
     constructor(id: string) {
         this.target = `notifications/${id}`;
+    }
+
+    public async markAsRead() {
+        return this.task(new Task("mark_read"));
+    }
+
+    public async task(t: Task<CollectionActions>, query?: ApiRequest.QueryParams): Promise<Task<CollectionActions>> {
+        return ApiRequest._post<Task<CollectionActions>>(
+            `${this.target}/tasks`,
+            t,
+            query
+        );
     }
 }
