ede9cceb9d7ca2acb3660d96641a1b98c87f8be8
Alex Johnson <me@alex-johnson.net>
2016-10-07 13:48:47 -0400

    change_tier to apply for teams

--- src/modules/teams/teams.ts
+++ src/modules/teams/teams.ts
@@ -60,7 +60,7 @@ export interface UpdateParams {
     };
 }
 
-export type SingleActions = "change_tier";
+export type SingleActions = "apply";
 
 export class CollectionRequest {
     private static target = "teams";
@@ -104,7 +104,7 @@ export class SingleRequest {
     }
 
     public async changeTier(tier: string) {
-        return this.tasks().create("change_tier", { tier: tier });
+        return this.tasks().create("apply", { tier: tier });
     }
 
     public tasks() {
