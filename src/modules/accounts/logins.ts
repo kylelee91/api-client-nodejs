79d65fc0cd4d65676ab11676a7fa6dc7dd2d0e31
Alex Johnson <home@alex-johnson.net>
2016-11-10 12:09:49 -0500

    logins -> login

--- src/modules/accounts/logins.ts
+++ src/modules/accounts/logins.ts
@@ -1,14 +1,14 @@
 import { CollectionDoc, SingleDoc, Resource, ResourceId, Time } from "../../common/structures";
 
 export interface Collection extends CollectionDoc {
-    data: Logins[];
+    data: Login[];
 }
 
 export interface Single extends SingleDoc {
-    data: Logins | null;
+    data: Login | null;
 }
 
-export interface Logins extends Resource {
+export interface Login extends Resource {
     account: string;
     time: Time;
     success: string;
