f761ffa50f81f0ac36927788bfefe3bb1de11412
Alex Johnson <home@alex-johnson.net>
2016-12-01 17:13:47 -0500

    remove unneeded stuffs from environments update and fixed cache reset

--- src/modules/environments/environments.ts
+++ src/modules/environments/environments.ts
@@ -9,8 +9,7 @@ import {
     Events,
     NewTask,
     Task,
-    Scope,
-    Time
+    Scope
 } from "../../common/structures";
 
 export function document(): typeof CollectionRequest;
@@ -69,8 +68,8 @@ export type Actions = "start" | "stop";
 
 export interface NewParams {
     name: string;
-    about: {
-        description: string;
+    about?: {
+        description?: string;
     };
 }
 
@@ -78,14 +77,6 @@ export interface UpdateParams {
     name?: string;
     about?: {
         description?: string;
-        developer?: {
-            name?: string;
-            website?: string;
-            organization?: string;
-        };
-        version?: string;
-        release_date?: Time;
-        documentation_url?: string;
     };
 }
 
