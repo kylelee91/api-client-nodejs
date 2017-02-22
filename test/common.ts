e954a05ea525b001cc9f8a6033c8860d55ba0509
amattoni <amattoni@petrichor.io>
2017-02-21 16:47:21 -0800

    example tests

---
+++ test/common.ts
@@ -0,0 +1,12 @@
+export interface IdResults {
+    // For functions where you'll need the ID of the resource generated
+    id: string;
+    results: Results;
+}
+
+export interface Results {
+    // Standard console output for EVERY request 
+    message: string;
+    status: number;
+    //...etc
+}
